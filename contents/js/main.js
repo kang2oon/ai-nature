a8b1b078dae7310d885f2ea3492ef8ed4c94c1b6
kang2oon <kang2oon@gmail.com>
2018-01-29 10:04:54 +0900

    final modify

--- contents/js/main.js
+++ contents/js/main.js
@@ -215,7 +215,7 @@
 				
 			}
 
-		} , { offset: '50%' } );
+		} , { offset: '80%' } );
 	};
 
 
@@ -252,39 +252,39 @@
 	
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Dec 02, 2017 15:37:25").getTime();
+		// var countDownDate = new Date("Dec 02, 2017 15:37:25").getTime();
 
 		// Update the count down every 1 second
-		var x = setInterval(function() {
+		// var x = setInterval(function() {
 
 		// Get todays date and time
-		var now = new Date().getTime();
+		// var now = new Date().getTime();
 
 		// Find the distance between now an the count down date
-		var distance = countDownDate - now;
+		// var distance = countDownDate - now;
 
 		// Time calculations for days, hours, minutes and seconds
-		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		// var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		// var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		// var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		// var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
 		// Display the result in an element with id="demo"
 		// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
 		// + minutes + "Minutes " + seconds + "Seconds ";
 
 		// Display the result in an element with id="demo"
-		document.getElementById("days").innerHTML = days +" <small>days</small>";
-		document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
-		document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-		document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
+		// document.getElementById("days").innerHTML = days +" <small>days</small>";
+		// document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
+		// document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
+		// document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
 
 		// If the count down is finished, write some text 
-		if (distance < 0) {
-		 clearInterval(x);
-		 document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
-		}
-		}, 1000);	
+		// if (distance < 0) {
+		//  clearInterval(x);
+		//  document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
+		// }
+		// }, 1000);	
 	
 		
 	var bgVideo = function() {
